fb0df2613c0b2e862f7d61c2a1b3cd6cabb2f3ff
csm0222 <csm0222@naver.com>
2022-12-03 02:24:26 +0900

    코틀린완벽가이드 04챕터 연습II

--- src/main/kotlin/04/main0401.kt
+++ src/main/kotlin/04/main0401.kt
@@ -1,10 +1,12 @@
-class Person(firstName: String, familyName: String) {
-    var firstName: String = ""
-    var familyName: String = ""
+import java.lang.IllegalArgumentException
+
+class Person(firstName: String, familyName: String = "") {
+    var firstName: String = firstName
+    var familyName: String = familyName
     var age: Int = 0
 
     val fullName = "$firstName $familyName"
-
+    fun fullName2() = "$firstName - $familyName"
     fun showMe() {
         println("${fullName}: $age")
     }
@@ -23,7 +25,48 @@ fun showAge(p: Person) = println(p.age)
 fun readAge(p: Person) {
     p.age = readLine()!!.toInt()
 }
+
+class Room(vararg val persons: Person) {
+    fun showNames(){
+        for (person in persons) println(person.fullName2())
+    }
+}
+
+class Person2 {
+    val firstName: String
+    val familyName: String
+    constructor(firstName: String, familyName: String) {
+        this.firstName = firstName
+        this.familyName = familyName
+    }
+
+    constructor(fullName: String) {
+        val names = fullName.split(" ")
+        if (names.size != 2) {
+            throw IllegalArgumentException("Invalid name: $fullName")
+        }
+
+        firstName = names[0]
+        familyName = names[1]
+    }
+}
+class Person3 {
+    val fullName: String
+    constructor(firstName: String, familyName: String) : this("$firstName $familyName")
+
+    constructor(fullName: String) {
+        this.fullName = fullName
+    }
+}
+class Person4(val fullName: String) {
+    constructor(firstName: String, familyName: String) : this("$firstName $familyName")
+
+}
 fun main() {
     val person = Person("Choi", "Seongmin")
     println( person.fullName )
+    println( person.fullName2() )
+
+    val room = Room(Person("choi"), Person("csm", "0222"))
+    room.showNames()
 }
