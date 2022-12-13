740c718d4f87f0b6512696ae00054911c88efcd6
csm0222 <csm0222@naver.com>
2022-12-14 03:10:59 +0900

    코틀린완벽가이드 04챕터 연습V (4.2.2 널가능성과 스마트캐스트)

--- src/main/kotlin/04/main0404.kt
+++ src/main/kotlin/04/main0404.kt
@@ -12,6 +12,25 @@ fun exclaim(s: String?) {
     println(s + "!")
 }
 
+fun isLetterString2(s: String?): Boolean {
+    if (s == null) return false
+
+    if (s.isEmpty()) return false
+
+    for( ch in s ) {
+        if (!ch.isLetter()) return false
+    }
+
+    return true
+}
+
+fun describeNumber(n: Int?) = when(n) {
+    null -> "null"
+    in 0..10 -> "small"
+    in 11..100 -> "large"
+    else -> "out of range"
+}
+
 fun main(argv: Array<String>) {
     println(isLetterString("abc"))
     println(isBooleanString(null))
