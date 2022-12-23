131a4916ecca807a4843cfeec7eeeab101aff62e
csm0222 <csm0222@naver.com>
2022-12-24 00:58:54 +0900

    코틀린완벽가이드 05챕터 연습II

--- src/main/kotlin/05/main0503.kt
+++ src/main/kotlin/05/main0503.kt
@@ -4,8 +4,30 @@ fun sum3(numbers: IntArray) =
 fun max3(numbers: IntArray) =
     aggregate(numbers, { result, op -> if (op > result ) op else result })
 
+fun check(s: String, condition: (Char) -> Boolean): Boolean{
+    for (c in s ) {
+        if (!condition(c)) return false
+    }
+    return true
+}
+
+fun check2(s: String, condition: (Int, Char) -> Boolean): Boolean {
+    for (i in s.indices) {
+        if (!condition(i, s[i])) return false
+    }
+    return true
+}
 
 fun main(argv: Array<String>) {
     println(sum3(intArrayOf(1, 2, 3)))
     println(max3(intArrayOf(1, 2, 3)))
+
+    val time = measureTime{ 1 + 2 }
+    println(time)
+
+    println(check("Hello") { c -> c.isLetter()})
+    println(check("hello") { it.isLowerCase()})
+
+    println(check2("Hello") {_, c->c.isLetter()})
+    println(check2("Hello") {i, c-> i == 0 || c.isLowerCase()})
 }
